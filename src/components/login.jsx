48bc966c65d24b7d14bcfdc873323103c02ade95
Syedmoiz7 <syedzada7866@gmail.com>
2022-11-23 11:53:36 +0500

    two more files

---
+++ src/components/login.jsx
@@ -0,0 +1,8 @@
+function Login() {
+    return(
+    <div>
+        This is Login page
+    </div>)
+}
+
+export default Login;
---
+++ src/components/signup.jsx
@@ -0,0 +1,8 @@
+function Signup() {
+    return(
+    <div>
+        This is Signup page
+    </div>)
+}
+
+export default Signup;
